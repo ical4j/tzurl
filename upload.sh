70de97f64b0b3f52e9de4fe87aba8ea3bd007a1b
Ben Fortuna <bfortuna@odecee.com.au>
2015-04-17 02:00:30 +1000

    Improve script security

--- upload.sh
+++ upload.sh
@@ -1,4 +1,5 @@
+#!/usr/bin/env bash
 #scp -rp zoneinfo modularity@tzurl.org:tzurl.org
-rsync -e "ssh -o 'StrictHostKeyChecking no'" -av zoneinfo modularity@tzurl.org:tzurl.org
-rsync -e "ssh -o 'StrictHostKeyChecking no'" -av zoneinfo-outlook modularity@tzurl.org:tzurl.org
+rsync -e ssh -av zoneinfo modularity@tzurl.org:tzurl.org
+rsync -e ssh -av zoneinfo-outlook modularity@tzurl.org:tzurl.org
 
