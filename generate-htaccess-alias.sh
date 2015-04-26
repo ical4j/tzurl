cbdb9b7764afd11684c67439593c9106a8aeb509
Ben Fortuna <bfortuna@odecee.com.au>
2015-04-26 17:25:45 +1000

    automatically generate aliases

--- generate-htaccess-alias.sh
+++ generate-htaccess-alias.sh
@@ -3,5 +3,5 @@
 #
 ## Latest tzdata available here: ftp://elsie.nci.nih.gov/pub/
 #
-awk '/Link/ {print "RewriteRule","^(.*)"$3,"$1"$2,"[NC]"}' tzdata$TZDATA_RELEASE/backward > zoneinfo/.htaccess
-awk '/Link/ {print "RewriteRule","^(.*)"$3,"$1"$2,"[NC]"}' tzdata$TZDATA_RELEASE/backward > zoneinfo-outlook/.htaccess
+awk '/Link/ {print "RewriteRule","^(.*)"$3,"$1"$2,"[NC]"}' tzdata$TZDATA_RELEASE/backward >> zoneinfo/.htaccess
+awk '/Link/ {print "RewriteRule","^(.*)"$3,"$1"$2,"[NC]"}' tzdata$TZDATA_RELEASE/backward >> zoneinfo-outlook/.htaccess
