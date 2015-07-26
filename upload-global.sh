cfa6d12c12b33826401f47425a8b0582d0baed02
Ben Fortuna <bfortuna@odecee.com.au>
2015-07-26 23:27:30 +1000

    delete redundant files on sync

--- upload-global.sh
+++ upload-global.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-rsync -e ssh -av zoneinfo-global $SSH_USER@tzurl.org:tzurl.org
-rsync -e ssh -av zoneinfo-outlook-global $SSH_USER@tzurl.org:tzurl.org
+rsync -e ssh -av --delete zoneinfo-global $SSH_USER@tzurl.org:tzurl.org
+rsync -e ssh -av --delete zoneinfo-outlook-global $SSH_USER@tzurl.org:tzurl.org
 
