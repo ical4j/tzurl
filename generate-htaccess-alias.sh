d68a3e2da315e511ce2a63a95d8f854c1ae545cb
Ben Fortuna <benfortuna@users.noreply.github.com>
2016-01-18 12:03:36 +1100

    Remove rewrites that cause infinite looping

--- generate-htaccess-alias.sh
+++ generate-htaccess-alias.sh
@@ -4,3 +4,6 @@
 ## Latest tzdata available here: ftp://elsie.nci.nih.gov/pub/
 #
 awk '/Link/ {print "RewriteRule","^(.*)"$3,"$1"$2,"[NC]"}' tzdata$TZDATA_RELEASE/backward >> htaccess.tzurl
+
+# Remove rewrites that cause infinite loop..
+sed -r -i.bak '/\*\)(Kwajalein)(Singapore)|(UCT)|(UTC)/d' htaccess.tzurl
