2647e445736d42fdc24531618c29c5a19adb4623
Ben Fortuna <benfortuna@gmail.com>
2018-05-09 20:42:34 +1000

    Updated deploy host

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #scp -rp zoneinfo $SSH_USER@tzurl.org:tzurl.org
-rsync -e ssh -av --delete zoneinfo $SSH_USER@tzurl.org:www2.tzurl.org
-rsync -e ssh -av --delete zoneinfo-outlook $SSH_USER@tzurl.org:www2.tzurl.org
+rsync -e ssh -av --delete zoneinfo $SSH_USER@mnode.org:www2.tzurl.org
+rsync -e ssh -av --delete zoneinfo-outlook $SSH_USER@mnode.org:www2.tzurl.org
 
