dd5afcaa31729b74143b03f4229f4a98a92e733b
Ben Fortuna <benfortuna@gmail.com>
2018-10-10 22:02:22 +1100

    Fixed infinite loop for Jamaica

--- generate-htaccess-alias.sh
+++ generate-htaccess-alias.sh
@@ -6,4 +6,4 @@
 awk '/Link/ {print "RewriteRule","^(.*)"$3,"$1"$2,"[NC]"}' tzdata$TZDATA_RELEASE/backward >> htaccess.tzurl
 
 # Remove rewrites that cause infinite loop..
-sed -r -i.bak '/\*\)(Kwajalein)|(Singapore)|(UCT)|(UTC)/d' htaccess.tzurl
+sed -E -i.bak '/\*\)(Kwajalein)|\*\)(Jamaica)|(Singapore)|(UCT)|(UTC)/d' htaccess.tzurl
