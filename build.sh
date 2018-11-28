7db0d55af1cdae4fcc282f1fc45e6578ff5b6087
Ben Fortuna <benfortuna@gmail.com>
2018-11-28 20:54:03 +1100

    Support tzdata from non-standard locations

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-mkdir -p tzdata$TZDATA_RELEASE && curl -L http://www.iana.org/time-zones/repository/releases/tzdata$TZDATA_RELEASE.tar.gz | tar -zxC tzdata$TZDATA_RELEASE &&  make clean && make
+TZDATA_BASE_URL=${TZDATA_BASE_URL:-http://www.iana.org/time-zones/repository/releases/}
+mkdir -p tzdata$TZDATA_RELEASE && curl -L ${TZDATA_BASE_URL}tzdata$TZDATA_RELEASE.tar.gz | tar -zxC tzdata$TZDATA_RELEASE &&  make clean && make
 ./generate-zoneinfo.sh
 ./generate-zoneinfo-global.sh
 ./generate-htaccess-alias.sh
