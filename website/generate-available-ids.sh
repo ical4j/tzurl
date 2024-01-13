7c33ec7d6ea3973498150899a6a3cf93c839282f
Ben Fortuna <fortuna@micronode.com>
2024-01-13 11:43:22 +1100

    Fixed available ids generation

--- website/generate-available-ids.sh
+++ website/generate-available-ids.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-find $1 -mindepth 1 -type f -name *.ics| sort | cut -d / -f 2- | cut -d . -f 1 > $1/tz.availableIds
+find $1 -mindepth 1 -type f -name *.ics| sort | cut -d / -f 3- | cut -d . -f 1 > $1/tz.availableIds
